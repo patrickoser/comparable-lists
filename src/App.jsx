5d7a342040d09c1865d237b6f3abbe20e985e406
Patrick Oser <patrick.oser148@gmail.com>
2024-04-25 10:15:50 -0400

    Setup App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+import Signup from './components/signup'
+import Login from "./components/login"
+import LoginSignupHub from "./components/loginSignupHub"
 
 function App() {
 
+
   return (
     <main>
-      <h1 className='text-red-500 text-xl'>Tailwind with react with vite.</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LoginSignupHub />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} /> 
+        </Routes>
+      </Router>
     </main>
   )
 }
