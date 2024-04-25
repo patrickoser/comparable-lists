078be1ca616ebfc0710e2e159a13fe109b0e3dfb
Patrick Oser <patrick.oser148@gmail.com>
2024-04-25 10:28:04 -0400

    Setup LoginSignupHub.jsx.

---
+++ src/components/LoginSignupHub.jsx
@@ -0,0 +1,12 @@
+import Login from './login.jsx'
+import Signup from './signup.jsx'
+
+const LoginSignupHub = () => {
+    return (
+        <main>
+            <Signup />
+        </main>
+    )
+}
+
+export default LoginSignupHub
