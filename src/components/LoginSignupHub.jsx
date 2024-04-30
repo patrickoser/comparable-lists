cbf306b50077cb3de846c38bd8ba7ebbbbd1309d
Patrick Oser <patrick.oser148@gmail.com>
2024-04-30 13:03:00 -0400

    Add comments to LoginSignupHub.

--- src/components/LoginSignupHub.jsx
+++ src/components/LoginSignupHub.jsx
@@ -1,6 +1,8 @@
 import Signup from './Signup.jsx'
 
 const LoginSignupHub = () => {
+    // Add a feature that checks local storage to see if they
+    // already have an account before redirecting to Signup.
     return (
         <main>
             <Signup />
