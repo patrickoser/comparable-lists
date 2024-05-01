4101e3e26a3d3304f2db2f363a1d31ba38014013
Patrick Oser <patrick.oser148@gmail.com>
2024-05-01 12:47:02 -0400

    Add style to confirm password.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -44,15 +44,19 @@ const Signup = () => {
                             />
                         </div>
                     </div>
-                    <label for="password">Confirm password</label>
-                    <input 
-                        type="password"
-                        placeholder="Confirm password..."
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                        className="border"
-                    />
+                    <div>
+                        <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Confirm password</label>
+                        <div className="mt-2">
+                            <input 
+                                type="password"
+                                placeholder="Confirm password..."
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
                     <section className="flex justify-center">
                         <button type="submit" className="mr-1">Signup</button>
                         <Link to="/login"><button className="ml-1">Login</button></Link>
