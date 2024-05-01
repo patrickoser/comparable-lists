4c8bc1c9dedc637a84901d82ea28ef4693fbb47b
Patrick Oser <patrick.oser148@gmail.com>
2024-05-01 12:55:25 -0400

    Add style to submit button.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -57,14 +57,13 @@ const Signup = () => {
                             />
                         </div>
                     </div>
-                    <section className="flex justify-center">
-                        <button type="submit" className="mr-1">Signup</button>
-                        <p class="mt-10 text-center text-sm text-gray-500">
-                            Already have an account?
-                            <Link to="/login"><button className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login here!</button></Link>
-                        </p>
-                        
-                    </section>
+                    <div>
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Signup</button>
+                    </div>
+                    <p class="mt-10 text-center text-sm text-gray-500">
+                        Already have an account?
+                        <Link to="/login"><button className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login here!</button></Link>
+                    </p>
                 </form>
             </div>
         </main>
