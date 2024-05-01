e054ef36df9525af680f2ae5c021f5916a42b253
Patrick Oser <patrick.oser148@gmail.com>
2024-05-01 12:43:27 -0400

    Add styles to email and password of the form.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -13,48 +13,52 @@ const Signup = () => {
 
     return (
         <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <section>
-                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Not Signed Up Yet?</h2>
-                </div>
-                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form onSubmit={handleSignup} className="space-y-6">
-                        <section className="flex flex-col p-4 gap-2">
-                            <label for="email">Email address</label>
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Not Signed Up Yet?</h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleSignup} className="space-y-6">
+                    <div>
+                        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                        <div>
                             <input 
                                 type="email"
                                 placeholder="Enter email..."
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 required
-                                className="border"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
-                            <label for="password">Password</label>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        <div className="mt-2">
                             <input 
                                 type="password"
                                 placeholder="Enter password..."
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                                 required
-                                className="border"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
-                            <label for="password">Confirm password</label>
-                            <input 
-                                type="password"
-                                placeholder="Confirm password..."
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                                required
-                                className="border"
-                            />
-                        </section>
-                        <section className="flex justify-center">
-                            <button type="submit" className="mr-1">Signup</button>
-                            <Link to="/login"><button className="ml-1">Login</button></Link>
-                        </section>
-                    </form>
-                </div>
-            </section>
+                        </div>
+                    </div>
+                    <label for="password">Confirm password</label>
+                    <input 
+                        type="password"
+                        placeholder="Confirm password..."
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                        className="border"
+                    />
+                    <section className="flex justify-center">
+                        <button type="submit" className="mr-1">Signup</button>
+                        <Link to="/login"><button className="ml-1">Login</button></Link>
+                    </section>
+                </form>
+            </div>
         </main>
     )
 }
