daa45056eb4ac2e124cd2fc4e76a2b189793800a
Patrick Oser <patrick.oser148@gmail.com>
2024-05-02 11:36:00 -0400

    Small change to Signup h2.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -14,7 +14,7 @@ const Signup = () => {
     return (
         <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Not Signed Up Yet?</h2>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Not signed up yet?</h2>
             </div>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form onSubmit={handleSignup} className="space-y-6">
