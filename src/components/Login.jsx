28bfc27088590a797617237ae545d12a3a626c31
Patrick Oser <patrick.oser148@gmail.com>
2024-05-02 11:42:54 -0400

    Add div that wraps form in Login and style it.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,30 +14,32 @@ const Login = () => {
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account.</h2>
             </div>
-            <form onSubmit={handleLogin}>
-                <section>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Enter email..."
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Enter password..."
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </section>
-                <section>
-                    <button type="submit">Login</button>
-                    <Link to="/signup"><button>Signup</button></Link>
-                </section>
-            </form>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleLogin} className="">
+                    <section>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Enter email..."
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Enter password..."
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </section>
+                    <section>
+                        <button type="submit">Login</button>
+                        <Link to="/signup"><button>Signup</button></Link>
+                    </section>
+                </form>
+            </div>
         </main>
     )
 }
