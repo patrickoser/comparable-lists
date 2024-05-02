abff84ee67f67e63e4affee0a2d1a7ed0bf4fbfa
Patrick Oser <patrick.oser148@gmail.com>
2024-05-02 14:16:28 -0400

    Add autocomplete attribute to Signup.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -26,6 +26,7 @@ const Signup = () => {
                                 placeholder="Enter email..."
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
+                                autoComplete="email"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
@@ -39,6 +40,7 @@ const Signup = () => {
                                 placeholder="Enter password..."
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
+                                autoComplete="new-password"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
@@ -52,6 +54,7 @@ const Signup = () => {
                                 placeholder="Confirm password..."
                                 value={confirmPassword}
                                 onChange={(e) => setConfirmPassword(e.target.value)}
+                                autoComplete="new-password"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
