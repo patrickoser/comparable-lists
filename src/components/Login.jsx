cf05beda3fc26a97c8c1caaa76c1037106775343
Patrick Oser <patrick.oser148@gmail.com>
2024-05-02 13:09:39 -0400

    Move Signup button in Login to buttom and style a p tag around it.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -53,9 +53,12 @@ const Login = () => {
                     </div>
                     <div>
                         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign In</button>
-                        <Link to="/signup"><button>Signup</button></Link>
                     </div>
                 </form>
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Don't have an account? 
+                    <Link to="/signup"><button>Signup here!</button></Link>
+                </p>
             </div>
         </main>
     )
