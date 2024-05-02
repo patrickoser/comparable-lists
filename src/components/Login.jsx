fc382772950a5dc4e792c65442213b24cdb2f08f
Patrick Oser <patrick.oser148@gmail.com>
2024-05-02 11:41:00 -0400

    Style h2 tag in Login.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     return (
         <main className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2>Sign in to your account.</h2>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account.</h2>
             </div>
             <form onSubmit={handleLogin}>
                 <section>
