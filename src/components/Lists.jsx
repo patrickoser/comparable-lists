63dd0648c59488c113350b50ffa70d394db8de8c
Patrick Oser <patrick.oser148@gmail.com>
2024-05-03 09:38:09 -0400

    Add bp to Lists.

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+
 // Will pull in users lists to populate their profile page
+
+const Lists = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Lists
