963c466d18585f4f90b1d99911f42707164157e5
Patrick Oser <patrick.oser148@gmail.com>
2024-05-03 12:33:42 -0400

    Add comments to List.

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+// A list of products that is created by the user and added to
+// their lists on their Profile
+
 const List = () => {
     return (
         <div></div>
