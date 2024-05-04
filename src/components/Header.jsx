e08869471839b84dfac9ee5b5793ce71ca1c7e9a
Patrick Oser <patrick.oser148@gmail.com>
2024-05-04 16:41:40 -0400

    Create file for Header and Footer and move code into Header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <section>
+                <h2>Comparable</h2>
+            </section>
+            <nav>
+                <li><button>New List</button></li>
+                <li><button>Contact</button></li>
+                <li><button>Settings</button></li>
+            </nav>
+        </header>
+    )
+}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,16 +6,6 @@ import Lists from "./Lists"
 const Profile = () => {
     return (
         <div>
-            <header>
-                <section>
-                    <h2>Comparable</h2>
-                </section>
-                <nav>
-                    <li><button>New List</button></li>
-                    <li><button>Contact</button></li>
-                    <li><button>Settings</button></li>
-                </nav>
-            </header>
             <main>
                 <Lists />
             </main>
