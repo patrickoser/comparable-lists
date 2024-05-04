e3d4f38fc65929eb7f4797fb8871ba994ac374df
Patrick Oser <patrick.oser148@gmail.com>
2024-05-04 16:45:15 -0400

    Write bp for Footer and add my Github to it.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+                <a href="https://github.com/patrickoser">My Github</a>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
