4ddf3148d2bcd2f7b0ee43c2aa12148d74fc29bb
Patrick Oser <patrick.oser148@gmail.com>
2024-05-07 15:04:25 -0400

    Import Header and Footer in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
 import Signup from './components/Signup.jsx'
 import Login from "./components/Login.jsx"
 import LoginSignupHub from "./components/LoginSignupHub.jsx"
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
 
 function App() {
 
