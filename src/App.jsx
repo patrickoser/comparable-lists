bc405d810779769efbeb32c2e19b31463e7379d7
Patrick Oser <patrick.oser148@gmail.com>
2024-05-08 09:40:55 -0400

    Import Settings and Contact in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./components/Login.jsx"
 import LoginSignupHub from "./components/LoginSignupHub.jsx"
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
+import Settings from "./components/Contact.jsx"
+import Contact from "./components/Contact.jsx"
 
 function App() {
 
