5d32d5446c310fcde3b05e6bba7158996b1dfc6a
Patrick Oser <patrick.oser148@gmail.com>
2024-05-09 14:27:51 -0400

    Add return Link to h1 in Settings.

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Settings = () => {
     return (
         <div>
             <h1 className="text-center">Settings</h1>
+            <Link to="/"><h1 className="text-center">LoginSignupHub</h1></Link>
         </div>
     )
 }
