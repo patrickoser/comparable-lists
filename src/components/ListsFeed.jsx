db9baf492cf5e2766e650521cc52a55564efbad0
Patrick Oser <patrick.oser148@gmail.com>
2024-06-02 08:16:00 -0400

    Add comments to ListsFeed.

--- src/components/ListsFeed.jsx
+++ src/components/ListsFeed.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 const ListsFeed = () => {
     return (
+        // Set up axios to practice pulling in lists and feeding
+        // them into the appropriate components.
         <div></div>
     )
 }
